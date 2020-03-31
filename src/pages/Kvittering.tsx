183bc06be5bde2d6b98286c452742e5a72f6f3e3
MajaHelene <maja.helene.pedersen@nav.no>
2020-03-31 08:41:03 +0200

    Endre kvittering

--- src/pages/Kvittering.tsx
+++ src/pages/Kvittering.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AlertStripeInfo } from 'nav-frontend-alertstriper';
+import { Ingress } from 'nav-frontend-typografi';
 import { useAppStore } from '../data/store/AppStore';
 import { Link } from 'react-router-dom';
 
@@ -9,11 +10,20 @@ const Kvittering = () => {
   return (
     <div className="limit">
       <AlertStripeInfo>
-        <div>Søknaden er mottat</div>
-        <div>Referansnummer: {referanseNummer}</div>
+        <div>
+          <Ingress>Søknaden er mottatt.</Ingress>
+          Dersom den blir godkjent vil beløpet utbetales innen X virkedager. Beløpet kan ikke overstige 6G.
+        </div>
+        <div>
+          <Ingress>Referansenummer: <span style={{fontWeight: "bold"}}>{referanseNummer}</span></Ingress>
+          Ta vare på referansenummeret da du vil trenge det dersom du kontakter oss om denne saken.
+        </div>
         <Link to="/" className="lenke informasjonsboks__lenke">
-          Ny søknad
+          Opprett en ny søknad
         </Link>
+        <div><Link to="https://arbeidsgiver.nav.no/min-side-arbeidsgiver/" className="lenke informasjonsboks__lenke">
+          Tilbake til Min side - arbeidsgiver
+        </Link></div>
       </AlertStripeInfo>
     </div>
   );
