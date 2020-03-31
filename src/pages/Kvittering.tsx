7ef2c0deea0658bda0f84d479848a633b45098a2
MajaHelene <maja.helene.pedersen@nav.no>
2020-03-31 14:57:19 +0200

    Endre ordlyd i kvittering

--- src/pages/Kvittering.tsx
+++ src/pages/Kvittering.tsx
@@ -19,13 +19,12 @@ const Kvittering = () => {
                     <div>
                         <Ingress>Referansenummer: <b>{referanseNummer}</b></Ingress>
                         <Normaltekst>
-                            Ta vare på referansenummeret da du vil trenge det dersom du kontakter oss om denne saken.
-                            Du vil også ha behov for fødselsnummeret til de ansatte det gjelder.
+                            Ta vare på referansenummeret. Du trenger det hvis du kontakter oss om denne saken.
+                            Ha i tilfelle også den ansattes fødselsnummer klart.
                         </Normaltekst>
                     </div>
                     <AlertStripeInfo>
-                        Dersom dere ønsker å endre til mer hyppige utbetalinger enn det som er standard
-                        kan det gjøres via skjema for endring av utbetalingsintervaller på Altinn.
+                        Ønsker dere hyppigere utbetalinger enn vanlig? I Altinn kan dere endre utbetalingsintervallet.
                     </AlertStripeInfo>
                     <div><a id="logout"
                             href="https://loginservice.nav.no/slo" className="lenke informasjonsboks__lenke">Logg ut</a>
