2dc4a22b738d2f642cbb0f0c2dcb7408eb87b8c4
MajaHelene <maja.helene.pedersen@nav.no>
2020-04-07 14:30:01 +0200

    mer refaktorering

--- src/pages/KvitteringExcel.tsx
+++ src/pages/KvitteringExcel.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import {AlertStripeInfo} from 'nav-frontend-alertstriper';
-import {Ingress, Innholdstittel, Normaltekst} from 'nav-frontend-typografi';
+import {Innholdstittel, Normaltekst} from 'nav-frontend-typografi';
 import Panel from 'nav-frontend-paneler';
-import {useAppStore} from '../data/store/AppStore';
 import {Link} from 'react-router-dom';
 import './Kvittering.less';
 import Lenke from "nav-frontend-lenker";
 
 const KvitteringExcel = () => {
     return (
-        <div className="kvitteringExcel">
+        <div className="kvittering">
             <div className="limit">
                 <Panel>
                     <div>
@@ -22,13 +21,16 @@ const KvitteringExcel = () => {
                             Du trenger referansenummeret hvis du kontakter oss om saken.
                             Ha i tilfelle også den ansattes fødselsnummer klart.
                             <br/><br/>
-                            <Lenke href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/aa-registeret-og-a-meldingen/relatert-informasjon/bankkontonummer-refusjoner-fra-nav-til-arbeidsgiver">
+                            <Lenke
+                                href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/aa-registeret-og-a-meldingen/relatert-informasjon/bankkontonummer-refusjoner-fra-nav-til-arbeidsgiver">
                                 Har du ikke registrert kontonummer hos oss, må du gjøre det snarest.</Lenke>
                         </Normaltekst>
                     </div>
                     <AlertStripeInfo>
                         Ønsker dere hyppigere utbetalinger enn vanlig? &nbsp;
-                        <a href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546" className="lenke ">I Altinn kan dere endre utbetalingsintervallet.</a>
+                        <a href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546"
+                           className="lenke ">
+                            I Altinn kan dere endre utbetalingsintervallet.</a>
                     </AlertStripeInfo>
                     <div><a id="logout"
                             href="https://loginservice.nav.no/slo" className="lenke informasjonsboks__lenke">Logg ut</a>
