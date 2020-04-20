a881680a717dc8422420849f8dd88803113d9d56
Morten Laukvik <morten.laukvik@nav.no>
2020-04-20 10:44:51 +0200

    Hjelpetekste i egne klasser

---
+++ src/components/ansatte/HjelpetekstDager.tsx
@@ -0,0 +1,11 @@
+import Hjelpetekst from "nav-frontend-hjelpetekst";
+import React from "react";
+
+export const HjelpetekstDager = () => {
+    return (
+        <Hjelpetekst>
+            <li>Her teller du dagene det skulle vært utbetalt lønn fra og med dag 4 i arbeidsgiverperioden. Helger og helligdager kan tas med hvis de er en del av den faste arbeidstiden.</li>
+            <li>Var noen av fraværsdagene før 16. mars, kan du ikke ta dem med.</li>
+        </Hjelpetekst>
+    )
+}
---
+++ src/components/ansatte/HjelpetekstPeriode.tsx
@@ -0,0 +1,8 @@
+import Hjelpetekst from "nav-frontend-hjelpetekst";
+import React from "react";
+
+export const HjelpetekstPeriode = () => {
+    return (
+        <Hjelpetekst>Fra og med første, til og med siste fraværsdag i arbeidsgiverperioden</Hjelpetekst>
+    )
+}
