42f8288831070e83db918daaf06509691ccf4e74
Morten Laukvik <morten.laukvik@nav.no>
2020-04-20 12:27:32 +0200

    Ikke vis modal vindu når det er valideringsfeil

--- src/components/ansatte/Ansatte2.tsx
+++ src/components/ansatte/Ansatte2.tsx
@@ -12,63 +12,74 @@ import {HjelpetekstDager} from "./HjelpetekstDager";
 import {HjelpetekstPeriode} from "./HjelpetekstPeriode";
 import {Eklæring} from "./Erklæring";
 import {ValideringOppsummering} from "./ValideringOppsummering";
+import {ValideringsFeil} from "./ValideringsFeil";
 
 const Ansatte2 = ({arbeidsgiverId}) => {
-    const {ansatte, setAnsatte} = useAppStore();
-    const {feil, setFeil} = useAppStore();
-    const handleSubmit = async(e: React.FormEvent): Promise<void> => {
-      e.preventDefault();
-        const validerteAnsatte = Validering(ansatte)
-        setFeil(ByggValideringsFeil(validerteAnsatte))
-        if (IsValid(validerteAnsatte)){
-            const innsendteAnsatte = await Innsending(arbeidsgiverId, validerteAnsatte);
-            setAnsatte(innsendteAnsatte);
-        } else {
-            setAnsatte(validerteAnsatte);
-        }
-    };
-    return (
-        <>
-            <form onSubmit={handleSubmit} className="refusjonsform">
-            <table className="AnsattTable">
-                <tbody>
-                    <tr>
-                        <td>Rad</td>
-                        <td>
-                            <span>Fødselsnummer til ansatt:</span>
-                        </td>
-                        <td>
-                            <span>Hvilken periode var den ansatte borte?</span>
-                            {HjelpetekstPeriode()}
-                        </td>
-                        <td>
-                            <span>Antall dager det skulle vært utbetalt lønn</span>
-                            {HjelpetekstDager()}
-                        </td>
-                        <td>
-                            <span>Brutto beløp som  søkes refundert</span>
-                            {HjelpetekstRefusjon()}
-                        </td>
-                        <td></td>
-                    </tr>
-                {
-                    ansatte.map((ansatt, index) => AnsattRad(ansatt.id))
-                }
-                </tbody>
-            </table>
-            <LeggTilKnapp/>
+  const {ansatte, setAnsatte} = useAppStore();
+  const {feil, setFeil} = useAppStore();
+  const handleSubmit = async (e: React.FormEvent): Promise<void> => {
+    e.preventDefault();
+    const validerteAnsatte = Validering(ansatte)
+    if (IsValid(validerteAnsatte)) {
+      setFeil([])
+      const innsendteAnsatte = await Innsending(arbeidsgiverId, validerteAnsatte);
+      setFeil(ByggValideringsFeil(innsendteAnsatte));
+      setAnsatte(innsendteAnsatte);
+    } else {
+      setFeil(ByggValideringsFeil(validerteAnsatte))
+      setAnsatte(validerteAnsatte);
+    }
+  };
 
-            <ValideringOppsummering/>
+  const handleFjernFeil = (slettetElement: number) => {
+    const aktiveFeil = feil.filter((element: ValideringsFeil) => {
+      return element.skjemaelementId !== `fnr_${slettetElement}`;
+    });
 
-            <div className="container">
-                <Eklæring/>
-            </div>
-            <div className="container">
-                <BekreftKnapp/>
-            </div>
-            </form>
-        </>
-    );
+    setFeil(aktiveFeil);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="refusjonsform">
+        <table className="AnsattTable">
+          <tbody>
+          <tr>
+            <td>Rad</td>
+            <td>
+              <span>Fødselsnummer til ansatt:</span>
+            </td>
+            <td>
+              <span>Hvilken periode var den ansatte borte?</span>
+              {HjelpetekstPeriode()}
+            </td>
+            <td>
+              <span>Antall dager det skulle vært utbetalt lønn</span>
+              {HjelpetekstDager()}
+            </td>
+            <td>
+              <span>Brutto beløp som  søkes refundert</span>
+              {HjelpetekstRefusjon()}
+            </td>
+            <td></td>
+          </tr>
+          {
+            ansatte.map((ansatt, index) => AnsattRad(ansatt.id, handleFjernFeil))
+          }
+          </tbody>
+        </table>
+        <LeggTilKnapp/>
+
+        <ValideringOppsummering/>
+
+        <div className="container">
+          <Eklæring/>
+        </div>
+        <div className="container">
+          <BekreftKnapp/>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Ansatte2;
