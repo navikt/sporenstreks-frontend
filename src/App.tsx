6c61c4f284bb3e3a11eecfb68ea089e401ae3ee5
Morten Laukvik <morten@laukvik.no>
2020-04-21 13:27:28 +0200

    Bruk ExcelOpplasting og KvitteringExcel i route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import StoreProvider from './data/store/StoreProvider';
 import { Amplitude } from './components/amplitude/AmplitudeProvider';
 import Kvittering from './pages/Kvittering';
 import SykepengerBulk from './pages/SykepengerBulk';
+import ExcelOpplastning from "./pages/ExcelOpplastning";
+import KvitteringExcel from "./pages/KvitteringExcel";
 
 const App = () => {
   return (
@@ -20,6 +22,8 @@ const App = () => {
               <Route exact path="/" render={() => <Sykepenger />} />
               <Route exact path="/bulk" render={() => <SykepengerBulk />} />
               <Route exact path="/kvittering" render={() => <Kvittering />} />
+              <Route exact path="/excel" render={() => <ExcelOpplastning />} />
+              <Route exact path="/kvitteringExcel" render={() => <KvitteringExcel />} />
               <Route path="/" render={() => <Redirecter />} />
             </Switch>
           </I18nextProvider>
