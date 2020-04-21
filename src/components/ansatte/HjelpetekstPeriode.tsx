814e1f8559bedbe57071983ff430453f1d2853dc
Morten Laukvik <morten.laukvik@nav.no>
2020-04-21 15:18:47 +0200

    Reformatter hjelpetekster

--- src/components/ansatte/HjelpetekstPeriode.tsx
+++ src/components/ansatte/HjelpetekstPeriode.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import {PopoverOrientering} from "nav-frontend-popover";
 
 export const HjelpetekstPeriode = () => {
-    return (
-        <Hjelpetekst type={PopoverOrientering.Under}>Fra og med første, til og med siste fraværsdag i arbeidsgiverperioden</Hjelpetekst>
-    )
+  return (
+    <Hjelpetekst type={PopoverOrientering.Under}>Fra og med første, til og med siste fraværsdag i
+      arbeidsgiverperioden</Hjelpetekst>
+  )
 }
