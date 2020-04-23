36c92a363227f921b4c3225261f287ca233d1475
Jeppe Feld <jafeld@gmail.com>
2020-04-23 10:47:04 +0200

    Update checkbox label

--- src/components/ansatte/Erklaring.tsx
+++ src/components/ansatte/Erklaring.tsx
@@ -13,7 +13,7 @@ export const Erklaring = ({value, handleSetErklæring}: erklaringProps) => {
       onChange={() => handleSetErklæring(!value)}
       label="Vi erklærer at det ikke er søkt om omsorgspenger og at arbeidstakeren ikke er permittert.
       Kravet er basert på arbeidstakerens opplysninger om at arbeidstakeren enten er smittet av koronaviruset,
-      mistenkt smittet eller i lovpålagt karantene. Vær oppmerksom på at NAV kan foreta kontroller."
+      mistenkt smittet eller i lovpålagt karantene."
     />
   )
 };
