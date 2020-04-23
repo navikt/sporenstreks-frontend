c2e63085054e1408252930415d9c581e31d85b13
Morten Laukvik <morten.laukvik@nav.no>
2020-04-23 10:48:01 +0200

    Begrense antall samtidige rader for bulk

--- src/components/ansatte/LeggTilKnapp.tsx
+++ src/components/ansatte/LeggTilKnapp.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {useAppStore} from "../../data/store/AppStore";
 import {Flatknapp} from "nav-frontend-knapper";
 import {byggAnsatt} from "../../data/types/sporenstreksTypes";
+import {Normaltekst} from "nav-frontend-typografi";
 
 export const LeggTilKnapp = () => {
     const {ansatte, setAnsatte} = useAppStore();
@@ -9,6 +10,9 @@ export const LeggTilKnapp = () => {
         e.preventDefault();
         ansatte.push(byggAnsatt())
         setAnsatte([...ansatte]);
+    }
+    if (ansatte.length == 50) {
+      return (<Normaltekst>Det er ikke tillatt å sende inn flere enn 50 stk om gangen.</Normaltekst>)
     }
     return (<Flatknapp onClick={handleAddRad}>Legg til enda en ansatt</Flatknapp>)
 }
