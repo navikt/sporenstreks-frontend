4b7aa788639c6d5ef868ce163740f51933bfe9c8
Kent Fjellstad <kent@fjellstad.name>
2020-04-24 15:29:27 +0200

    Flere tester. Mest for dekningens skyld

--- src/components/ansatte/berikAnsatte.test.ts
+++ src/components/ansatte/berikAnsatte.test.ts
@@ -199,7 +199,7 @@ describe("berikAnsatte", () => {
     expect(berikAnsatte(ansatte, backendResponce)).toEqual(expected);
   })
 
-  it("should add validation errors and concattinate if multiple", () => {
+  it("should set the status for generic error", () => {
     const ansatte: Ansatt[] = [
       {
         id: 1,
@@ -289,6 +289,106 @@ describe("berikAnsatte", () => {
       {
         "fnr": "3",
         "fnrError": "Validation identitetsnummer error.",
+        "fom": "fom",
+        "id": 3,
+        "oppdatert": 3,
+        "status": 3,
+        "tom": "tom"
+      }
+    ];
+
+    expect(berikAnsatte(ansatte, backendResponce)).toEqual(expected);
+  })
+
+  it("should set the status for virksomhetsnummer", () => {
+    const ansatte: Ansatt[] = [
+      {
+        id: 1,
+        fnr: "1",
+        fom: "fom",
+        tom: "tom",
+        status: 1,
+        oppdatert: 1
+      },
+      {
+        id: 2,
+        fnr: "2",
+        fom: "fom",
+        tom: "tom",
+        status: 2,
+        oppdatert: 2
+      },
+      {
+        id: 3,
+        fnr: "3",
+        fom: "fom",
+        tom: "tom",
+        status: 3,
+        oppdatert: 3
+      }
+    ];
+
+    const backendResponce: BackendStatus[] = [
+      {
+        status: "OK",
+        validationErrors: null,
+        genericMessage: null,
+        referenceNumber: "1234"
+      },
+      {
+        status: "GENERIC_ERROR",
+        validationErrors: [
+          {
+            validationType: "type",
+            message: "Validation periode error",
+            propertyPath: 'perioder[0].tom',
+            invalidValue: "fom"
+          },
+          {
+            validationType: "type",
+            message: "Validation generic periode error",
+            propertyPath: 'perioder',
+            invalidValue: "fom"
+          }
+        ],
+        genericMessage: null,
+        referenceNumber: null
+      },
+      {
+        status: "VALIDATION_ERRORS",
+        validationErrors: [
+          {
+            validationType: "type",
+            message: "Validation virksomhetsnummer error.",
+            propertyPath: 'virksomhetsnummer',
+            invalidValue: "fom"
+          }
+        ],
+        genericMessage: null,
+        referenceNumber: null
+      },
+    ]
+
+    const expected: Ansatt[] = [
+      {
+        "fnr": "1",
+        "fom": "fom",
+        "id": 1,
+        "oppdatert": 1,
+        "referenceNumber": "1234",
+        "status": 2,
+        "tom": "tom"
+      },
+      {
+        "fnr": "2",
+        "fom": "fom",
+        "id": 2,
+        "oppdatert": 2,
+        "status": 4,
+        "tom": "tom"
+      },
+      {
+        "fnr": "3",
         "fom": "fom",
         "id": 3,
         "oppdatert": 3,
