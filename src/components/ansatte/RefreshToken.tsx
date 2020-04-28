6f53204ebaca0e1ae33e51fc7fd137cf008adc45
Kent Fjellstad <kent@fjellstad.name>
2020-04-28 15:04:33 +0200

    Bare forny når siden er i bruk

--- src/components/ansatte/RefreshToken.tsx
+++ src/components/ansatte/RefreshToken.tsx
@@ -4,17 +4,36 @@ import './RefreshToken.less';
 
 const RefreshToken = () => {
   const [displayIframe, setDisplayIframe] = useState(true);
-
+  const [haveInteraction, setHaveInteraction] = useState(true);
 
   const toggleState = () => {
     setDisplayIframe(displayIframe => !displayIframe);
+    setHaveInteraction(false);
+  }
+
+  const interactionHandler = () => {
+    setHaveInteraction(true);
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      toggleState();
+      if(haveInteraction) {
+        toggleState();
+      }
     }, 1200000);  // 20 minutter. Gir refresh av token hvert 40. minutt
     return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('keydown', interactionHandler);
+    window.addEventListener('mousemove', interactionHandler);
+    window.addEventListener('touchstart', interactionHandler);
+
+    return () => {
+      window.removeEventListener('keydown', interactionHandler);
+      window.removeEventListener('mousemove', interactionHandler);
+      window.removeEventListener('touchstart', interactionHandler);
+    };
   }, []);
 
   return <>{displayIframe && <iframe name="jwt-refresh-token-iframe" className={"refresh-token-jwt"} src={env.loginServiceUrl} />}</>
