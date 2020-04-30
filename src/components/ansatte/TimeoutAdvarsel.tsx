b1f99ff05f63824e6ca734c87e807e2248f19023
Jeppe Feld <jafeld@gmail.com>
2020-04-30 14:21:17 +0200

    TimeoutAdvarsel text update

--- src/components/ansatte/TimeoutAdvarsel.tsx
+++ src/components/ansatte/TimeoutAdvarsel.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ModalWrapper from 'nav-frontend-modal';
 import {AlertStripeAdvarsel} from 'nav-frontend-alertstriper';
-import {Undertittel} from "nav-frontend-typografi";
+import { Innholdstittel, Undertittel } from "nav-frontend-typografi";
 import './TimeoutAdvarsel.less';
 import Lenke from "nav-frontend-lenker";
 
@@ -15,14 +15,16 @@ const TimeoutAdvarsel = () => {
       isOpen={true}
       onRequestClose={() => setOpen(false)}
       closeButton={false}
-      className={"TimeoutAdvarsel"}
+      className={"timeout-advarsel"}
       contentLabel=""
     >
-      <AlertStripeAdvarsel>
-        <Undertittel>Du blir logget ut etter én time</Undertittel>
+      <AlertStripeAdvarsel className="timeout-advarsel__innhold">
+        <Innholdstittel>Du blir automatisk logget ut etter én time</Innholdstittel>
         <ul>
           <li>Én time etter at du logget inn hos NAV, blir du automatisk logget ut.</li>
-          <li>Tjenesten mellomlagrer dessverre ikke det du skriver</li>
+          <li>Tjenesten mellomlagrer dessverre ikke det du skriver.</li>
+          <li>Bli ferdig med skjema senest en time innen du logget inn.</li>
+          <li>Er du i tvil - logg ut og inn igjen for å ha lengst mulig tid til utfylling.</li>
         </ul>
         <Lenke className={""} href="#" onClick={() => setOpen(false)}>Lukk</Lenke>
       </AlertStripeAdvarsel>
