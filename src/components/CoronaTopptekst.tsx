d54dd0a18e18f16fd7e4730090555bd7900b5ba5
Aleksander Andresen <caeses@gmail.com>
2020-04-30 14:55:02 +0200

    Legg til informasjon om at ordningen ikke dekker stengte skoler ol

---
+++ src/components/CoronaTopptekst.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {Normaltekst, Undertittel} from "nav-frontend-typografi";
+import {Column, Row} from "nav-frontend-grid";
+import Panel from "nav-frontend-paneler";
+
+
+export const CoronaTopptekst = () => {
+  return (
+    <Row>
+      <Column>
+        <Panel>
+          <Normaltekst>
+            Når sykefraværet handler om korona, dekker NAV sykepenger fra dag 4 i de 16 dagene arbeidsgiveren vanligvis
+            skal
+            betale.
+            Den ansatte må være smittet, mistenkt smittet eller i pålagt karantene. Refusjon kan gis for dager fra og
+            med
+            16. mars.
+            <span> </span>
+            <a className="lenke informasjonsboks__lenke"
+               href="https://www.nav.no/no/bedrift/oppfolging/sykmeldt-arbeidstaker/nyheter/refusjon-av-sykepenger-ved-koronavirus--hva-er-status">
+              Se mer informasjon om refusjonsordningen.
+            </a>
+          </Normaltekst>
+          <Undertittel className="sykepenger--header">
+            Det kan ikke søkes om refusjon for fravær på grunn av stengte skoler eller barnehager
+          </Undertittel>
+        </Panel>
+      </Column>
+    </Row>
+  )
+}
