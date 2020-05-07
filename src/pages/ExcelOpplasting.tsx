9aae28380c6160f9adc45d139b95176e1364cf67
Maja <maja.helene.pedersen@nav.no>
2020-05-07 09:54:10 +0200

    Excelstruktur (#67)

--- src/pages/ExcelOpplasting.tsx
+++ src/pages/ExcelOpplasting.tsx
@@ -2,24 +2,25 @@ import React, {useState} from 'react';
 import 'nav-frontend-tabell-style';
 import {FormContext, useForm} from 'react-hook-form';
 import {Hovedknapp} from 'nav-frontend-knapper';
-import {Link, useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
-import {Ingress, Innholdstittel, Normaltekst} from 'nav-frontend-typografi';
+import {Ingress, Innholdstittel, Normaltekst, Undertittel} from 'nav-frontend-typografi';
 import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
-import {useAppStore} from '../data/store/AppStore';
 import {History} from 'history';
 import Vis from '../components/Vis';
 import env from '../util/environment';
 import './ExcelOpplasting.less';
 import Lenke from "nav-frontend-lenker";
 import excellogo from '../img/excel-logo.png';
-import { Erklaring } from '../components/ansatte/Erklaring';
-import { FeilTabell, tabellFeil } from '../components/feilvisning/FeilTabell';
+import {Erklaring} from '../components/ansatte/Erklaring';
+import {FeilTabell, tabellFeil} from '../components/feilvisning/FeilTabell';
 import InnloggetSide from "./InnloggetSide";
-
+import Panel from "nav-frontend-paneler";
+import Skillelinje from "../components/ansatte/Skillelinje";
+import {Column, Row} from "nav-frontend-grid";
 
 const ExcelOpplasting = () => {
-  const [ erklæringAkseptert, setErklæringAkseptert ] = useState<boolean>(false);
+  const [erklæringAkseptert, setErklæringAkseptert] = useState<boolean>(false);
   const methods = useForm();
   const {t} = useTranslation();
   const history: History = useHistory();
@@ -73,7 +74,7 @@ const ExcelOpplasting = () => {
             }
             case 200: {
               response.blob().then(data => {
-                history.push( '/kvitteringExcel')
+                  history.push('/kvitteringExcel')
                   setFeil([])
                 }
               )
@@ -115,81 +116,113 @@ const ExcelOpplasting = () => {
   };
 
   return (
-    <InnloggetSide>
-    <div className="excelopplasting">
-          <Ingress className="container">
-            Når sykefraværet handler om korona, dekker NAV sykepenger fra dag 4 i de 16 dagene
-            arbeidsgiveren vanligvis skal betale. Den ansatte må være smittet,
-            mistenkt smittet eller i pålagt karantene.
-            Refusjon kan gis for dager fra og med 16. mars.&nbsp;
-            <Lenke
-              href="https://www.nav.no/no/bedrift/oppfolging/sykmeldt-arbeidstaker/nyheter/refusjon-av-sykepenger-ved-koronavirus--hva-er-status">
-              Se mer informasjon om refusjonsordningen.
-            </Lenke>
-            <br/><br/>
-            <b>Det kan ikke søkes om refusjon for fravær på grunn av stengte skoler eller barnehager.</b>
-          </Ingress>
-          <div className="container limit">
+    <InnloggetSide className="excelopplasting">
+      <Row>
+        <Column>
+          <Panel>
+            <Ingress>
+              Når sykefraværet handler om korona, dekker NAV sykepenger fra dag 4 i de 16 dagene
+              arbeidsgiveren vanligvis skal betale. Den ansatte må være smittet,
+              mistenkt smittet eller i pålagt karantene.
+              Refusjon kan gis for dager fra og med 16. mars.&nbsp;
+              <Lenke
+                href="https://www.nav.no/no/bedrift/oppfolging/sykmeldt-arbeidstaker/nyheter/refusjon-av-sykepenger-ved-koronavirus--hva-er-status">
+                Se mer informasjon om refusjonsordningen.
+              </Lenke>
+            </Ingress>
+          </Panel>
+          <Panel>
+            <Undertittel>
+              Det kan ikke søkes om refusjon for fravær på grunn av stengte skoler eller barnehager.
+            </Undertittel>
+          </Panel>
+        </Column>
+      </Row>
+
+      <Skillelinje/>
+
+      <Row>
+        <Column>
+          <Panel>
             <Innholdstittel>Last ned Excel-malen, fyll ut og last opp.</Innholdstittel>
+          </Panel>
+          <Panel>
             <Normaltekst>
               Har du ansatte som har vært borte i to eller flere ikke-sammenhengende perioder
-              <Link to="/enkel" className="lenke">&nbsp;skal du bruke et eget skjema</Link>.
+              <Lenke href="/enkel">
+                &nbsp;skal du bruke et eget skjema
+              </Lenke>.
               Excel-opplasting er tiltenkt dere som har svært mange refusjonskrav.
-
               Vi har også et &nbsp;
-              <Link to="/bulk" className="lenke">
-                eget skjema for å søke om refusjonskrav for flere ansatte</Link>
+              <Lenke href="/bulk">
+                eget skjema for å søke om refusjonskrav for flere ansatte
+              </Lenke>
               &nbsp; dersom dere foretrekker å gjøre det på den måten.
             </Normaltekst>
-            <br/><br/>
+          </Panel>
+          <Panel>
             <Normaltekst>
               <img src={excellogo}
                    width="35"
                    className="excelopplasting__excellogo"
-              alt="Excel-symbol"/>
+                   alt="Excel-symbol"/>
               <Lenke href={env.baseUrl + "/api/v1/bulk/template"}>
                 Last ned malen her</Lenke>, og fyll ut.
               Det er ikke mulig å benytte ditt eget excel-dokument,
               alt må fylles ut i denne malen før du laster opp.
-            </Normaltekst>
-          </div>
-          <div className="container limit">
-            <Normaltekst>
               NB, det kan maks legges inn 5000 linjer per excel-doc.
               Om det ikke er tilstrekkelig, må dere gjøre dette i flere omganger.
             </Normaltekst>
+          </Panel>
+        </Column>
+      </Row>
+
+      <Skillelinje/>
+
+      <Row>
+        <Column>
+          <Panel>
             <label className="knapp filknapp">
               <input className="fileinput"
                      type="file"
                      id="fileUploader"
                      accept=".xls,.xlsx"
                      onChange={setUploadFile}
-                      onClick={(e: any) => e.target.value = null}/>
+                     onClick={(e: any) => e.target.value = null}/>
               {fileName}
             </label>
             <FeilTabell
               feil={feil}
               visAlleFeil={visAlleFeil}
               handleSetVisAlleFeil={visAlleFeil => setVisAlleFeil(visAlleFeil)}/>
-          </div>
-          <FormContext {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit)}
-                  className="excelform container limit"
-                  onClick={e => setHasTriedSubmit(true)}>
-            <Erklaring value={erklæringAkseptert} handleSetErklæring={value => setErklæringAkseptert(value)}/>
-              <Hovedknapp disabled={!(erklæringAkseptert && file !== undefined)} className="knapp filknapp">
-                Send søknad om refusjon</Hovedknapp>
-              <Vis hvis={hasTriedSubmit}>
-                <Vis hvis={!erklæringAkseptert}>
-                  <Normaltekst className="advarsel">Du må huke av erklæringen før du kan sende inn</Normaltekst>
+          </Panel>
+        </Column>
+      </Row>
+
+      <Skillelinje/>
+
+      <Row>
+        <Column>
+          <Panel>
+            <FormContext {...methods}>
+              <form onSubmit={methods.handleSubmit(onSubmit)}
+                    onClick={e => setHasTriedSubmit(true)}>
+                <Erklaring value={erklæringAkseptert} handleSetErklæring={value => setErklæringAkseptert(value)}/>
+                <Hovedknapp disabled={!(erklæringAkseptert && file !== undefined)} className="knapp filknapp">
+                  Send søknad om refusjon</Hovedknapp>
+                <Vis hvis={hasTriedSubmit}>
+                  <Vis hvis={!erklæringAkseptert}>
+                    <Normaltekst className="advarsel">Du må huke av erklæringen før du kan sende inn</Normaltekst>
+                  </Vis>
+                  <Vis hvis={file === undefined}>
+                    <Normaltekst className="advarsel">Du må laste opp Excel-skjemaet som skal sendes inn</Normaltekst>
+                  </Vis>
                 </Vis>
-                <Vis hvis={file === undefined}>
-                  <Normaltekst className="advarsel">Du må laste opp Excel-skjemaet som skal sendes inn</Normaltekst>
-                </Vis>
-              </Vis>
-            </form>
-          </FormContext>
-    </div>
+              </form>
+            </FormContext>
+          </Panel>
+        </Column>
+      </Row>
     </InnloggetSide>
   );
 };
