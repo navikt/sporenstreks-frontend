bd3e0054ee3ee5205507ba3b49502cadf1d72adf
Jeppe Feld <jafeld@gmail.com>
2020-05-11 12:12:46 +0200

    Rework komponent

--- src/components/dager/DagerInput.tsx
+++ src/components/dager/DagerInput.tsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Select } from 'nav-frontend-skjema';
-import { useAppStore } from '../../data/store/AppStore';
 import { validateNotNullAndPositive } from '../validering/validateNotNullAndPositive';
 import { HjelpetekstDager } from '../ansatte/HjelpetekstDager';
+import uuid from 'uuid/v4';
 
 interface DagerInputProps {
-  id: number | string
+  feilmelding?: string,
+  antallDagerMedRefusjon?: number,
+  handleChange: any,
+  id?: number
 }
 
-export const DagerInput = ({id}: DagerInputProps) => {
-  const {ansatte, setAnsatte} = useAppStore();
-  const a = ansatte.find(a => a.id === id);
-  const handleChange = (evt) => {
-    if (a) {
-      a.antallDagerMedRefusjon = parseInt(evt.target.selectedIndex);
-      a.dagerError = validateNotNullAndPositive(a.antallDagerMedRefusjon);
-    }
-    setAnsatte([...ansatte]);
-  };
+export const DagerInput = ({ feilmelding, antallDagerMedRefusjon, handleChange, id }: DagerInputProps) => {
+  const [lokalFeil, setLokalFeil] = useState<string | undefined>();
   const optionArr = Array.from(Array(14).keys());
 
+  const handleDagerBlur = (evt) => {
+    const feilmelding = validateNotNullAndPositive(evt.target.value);
+    setLokalFeil(feilmelding)
+  };
+
+  useEffect(() => {
+    setLokalFeil(feilmelding);
+  }, [feilmelding])
+
+  const componentId = "dager_".concat(id ? String(id) : uuid());
+
+  const feilmeldingstekst = feilmelding ? feilmelding : lokalFeil;
+
   return <Select
-    feil={a?.dagerError}
+    id={componentId}
+    feil={feilmeldingstekst}
     onChange={handleChange}
+    onBlur={handleDagerBlur}
     label={
       <div style={{display: 'flex'}}>
         Antall dager
         <HjelpetekstDager/>
       </div>}
-    selected={a?.antallDagerMedRefusjon}>
+    selected={antallDagerMedRefusjon}>
     {
       optionArr.map(optionValue => <option key={optionValue} value={optionValue}>
         {optionValue ? optionValue : '-'}
