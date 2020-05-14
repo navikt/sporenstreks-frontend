2a281ebafc266b7014bf97f3db4ac9a91b969865
Kent Fjellstad <kent@fjellstad.name>
2020-05-14 13:47:52 +0200

    Ny advarselstekst om en times innlogging

--- src/components/ansatte/TimeoutAdvarsel.tsx
+++ src/components/ansatte/TimeoutAdvarsel.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import ModalWrapper from 'nav-frontend-modal';
-import {AlertStripeAdvarsel} from 'nav-frontend-alertstriper';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
 import { Innholdstittel } from "nav-frontend-typografi";
 import './TimeoutAdvarsel.less';
 import Lenke from "nav-frontend-lenker";
 import TokenUtloper from "./TokenUtloper";
 
 const TimeoutAdvarsel = () => {
-  const [ isOpen, setOpen ] = useState(true);
-  if (!isOpen){
-       return (<></>)
+  const [isOpen, setOpen] = useState(true);
+  if (!isOpen) {
+    return null;
   }
   return (
     <ModalWrapper
@@ -20,12 +20,11 @@ const TimeoutAdvarsel = () => {
       contentLabel=""
     >
       <AlertStripeAdvarsel className="timeout-advarsel__innhold">
-        <Innholdstittel>Du blir automatisk logget ut etter én time</Innholdstittel>
+        <Innholdstittel>Du blir logget ut etter 60 minutter</Innholdstittel>
         <ul>
-          <li><strong>Denne innloggingen utløper kl: <TokenUtloper/></strong></li>
-          <li>Tjenesten mellomlagrer dessverre ikke det du skriver.</li>
-          <li>Gjør deg ferdig med det du holder på med innen timen er gått.</li>
-          <li>Del eventuelt opp det du skal gjøre i mindre bolker, og logg inn på nytt for å bli ferdig med alle.</li>
+          <li>Vi anbefaler at du gjør deg ferdig innen timen er gått.</li>
+          <li>Blir du logget ut, får du mer informasjon om hva du skal gjøre, slik at du ikke mister det du har skrevet.</li>
+          <li><strong>Denne innloggingen utløper kl: <TokenUtloper /></strong></li>
         </ul>
         <Lenke className={""} href="#" onClick={() => setOpen(false)}>Lukk</Lenke>
       </AlertStripeAdvarsel>
