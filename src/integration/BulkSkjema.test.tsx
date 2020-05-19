54b259ef9495a0309dfc724a6c90d1cd6d926655
Morten Laukvik <laukvik@users.noreply.github.com>
2020-05-19 21:54:12 +0200

    Integrasjonstester - skall (#91)

---
+++ src/integration/BulkSkjema.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+describe('Skjema: Bulk', () => {
+  it('skal kreve at brukeren er logget inn', () => {
+  })
+  it('skal vise link til Enkel skjema', () => {
+  })
+  it('skal vise link til Excel skjema', () => {
+  })
+
+
+  it('skal kunne velge fil med størrelse inntil MAX verdi', () => {
+  })
+  it('skal vise feilmelding dersom man velger filer over MAX verdi', () => {
+  })
+  it('skal vise feilmelding dersom man velger filer som ikke er XLS og XLSX', () => {
+  })
+
+  it('skal ikke kunne trykke HovedKnapp før gyldig fil er valgt', () => {
+  })
+  it('skal ikke kunne trykke HovedKnapp før Erklaring er akseptert', () => {
+  })
+  it('skal ikke kunne trykke HovedKnapp når feilmeldinger vises', () => {
+  })
+
+  it('skal håndtere unauthorized', () => {
+  })
+
+  it('skal håndtere timeout ved innsending', () => {
+  })
+  it('skal håndtere error ved innsending', () => {
+  })
+  it('skal håndtere valideringsfeil ved innsending', () => {
+  })
+  it('skal vise kvitteringsside ved gyldig innsending', () => {
+  })
+
+});
---
+++ src/pages/InnloggetSide.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+describe('InnloggetSide', () => {
+  it('skal ikke vise skjema dersom man ikke har rettigheter', () => {
+  })
+
+  it('skal vise link til MinSideArbeidsgiver', () => {
+  })
+
+  it('skal vise bedriftsvelger dersom man har rettigheter', () => {
+  })
+  it('skal vise under elementer dersom man har rettigheter', () => {
+  })
+  it('skal vise TimeoutAdvarsel dersom man har rettigheter', () => {
+  })
+
+  it('skal vise advarsel dersom man ikke har rettigheter', () => {
+  })
+  it('skal ikke vise under elementer dersom man ikke har rettigheter', () => {
+  })
+  it('skal ikke vise TimeoutAdvarsel dersom man ikke har rettigheter', () => {
+  })
+});
