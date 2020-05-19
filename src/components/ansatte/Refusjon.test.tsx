87670d1ed3c0cc65b87a4cc4e636fc185e87342a
Kent Fjellstad <kent@fjellstad.name>
2020-05-19 11:05:21 +0200

    Beløpsformatering

---
+++ src/components/ansatte/Refusjon.test.tsx
@@ -0,0 +1,207 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import { useAppStore } from "../../data/store/AppStore";
+
+import Refusjon from './Refusjon';
+import { Ansatt } from '../../data/types/sporenstreksTypes';
+
+const mockAnsatte: Ansatt[] = [
+  {
+    id: "ansatt1",
+    fnr: "1",
+    fom: "2020-02-02",
+    tom: "2020-03-03",
+    status: 123,
+    oppdatert: 1
+  },
+  {
+    id: 1,
+    fnr: "1",
+    fom: "2020-02-02",
+    tom: "2020-03-03",
+    status: 123,
+    oppdatert: 1,
+    dagerError: "DagerError"
+  }
+];
+
+jest.mock('../../data/store/AppStore');
+
+describe('Refusjon', () => {
+  it('shold display the component', () => {
+    const mockUseAppStore = useAppStore as jest.Mock
+
+    mockUseAppStore.mockReturnValue({
+      ansatte: mockAnsatte,
+      setAnsatte: jest.fn()
+    });
+
+    render(<Refusjon id="ansatt1" />);
+
+    expect(screen.getByText(/Beløp/)).toBeInTheDocument();
+  })
+
+  it('shold update the ansatte data', () => {
+    const mockUseAppStore = useAppStore as jest.Mock
+    const mockSetAnsatte = jest.fn();
+    const expected: Ansatt[] = [
+      {
+        "beloep": 5,
+        "beloepError": undefined,
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": "ansatt1",
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      },
+      {
+        "dagerError": "DagerError",
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": 1,
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      }
+    ];
+
+    mockUseAppStore.mockReturnValue({
+      ansatte: mockAnsatte,
+      setAnsatte: mockSetAnsatte
+    });
+
+    render(<Refusjon id="ansatt1" />);
+
+    const inputFelt = screen.getByRole('textbox');
+
+    fireEvent.change(inputFelt, { target: { value: "5" } });
+
+    expect(screen.getByText(/Beløp/)).toBeInTheDocument();
+    expect(mockSetAnsatte).toHaveBeenCalledWith(expected)
+  })
+
+  it('shold update the ansatte data with the latest valid data', () => {
+    const mockUseAppStore = useAppStore as jest.Mock
+    const mockSetAnsatte = jest.fn();
+    const expected: Ansatt[] = [
+      {
+        "beloep": 5,
+        "beloepError": undefined,
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": "ansatt1",
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      },
+      {
+        "dagerError": "DagerError",
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": 1,
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      }
+    ];
+
+    mockUseAppStore.mockReturnValue({
+      ansatte: mockAnsatte,
+      setAnsatte: mockSetAnsatte
+    });
+
+    render(<Refusjon id="ansatt1" />);
+
+    const inputFelt = screen.getByRole('textbox');
+
+    fireEvent.change(inputFelt, { target: { value: "5" } });
+
+    fireEvent.change(inputFelt, { target: { value: "7kroner" } });
+
+    expect(screen.getByText(/Beløp/)).toBeInTheDocument();
+    expect(mockSetAnsatte).toHaveBeenCalledWith(expected)
+  })
+
+  it('shold update the ansatte data with formated 1000s', () => {
+    const mockUseAppStore = useAppStore as jest.Mock
+    const mockSetAnsatte = jest.fn();
+    const expected: Ansatt[] = [
+      {
+        "beloep": 34567,
+        "beloepError": undefined,
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": "ansatt1",
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      },
+      {
+        "dagerError": "DagerError",
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": 1,
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      }
+    ];
+
+    mockUseAppStore.mockReturnValue({
+      ansatte: mockAnsatte,
+      setAnsatte: mockSetAnsatte
+    });
+
+    render(<Refusjon id="ansatt1" />);
+
+    const inputFelt = screen.getByRole('textbox');
+
+    fireEvent.change(inputFelt, { target: { value: "34 567" } });
+
+    expect(screen.getByText(/Beløp/)).toBeInTheDocument();
+    expect(mockSetAnsatte).toHaveBeenCalledWith(expected)
+  })
+
+  it('shold give an error if the amount is too high', () => {
+    const mockUseAppStore = useAppStore as jest.Mock
+    const mockSetAnsatte = jest.fn();
+    const expected: Ansatt[] = [
+      {
+        "beloep": 1234567,
+        "beloepError": "Beløpet er for høyt",
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": "ansatt1",
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      },
+      {
+        "dagerError": "DagerError",
+        "fnr": "1",
+        "fom": "2020-02-02",
+        "id": 1,
+        "oppdatert": 1,
+        "status": 123,
+        "tom": "2020-03-03"
+      }
+    ];
+
+    mockUseAppStore.mockReturnValue({
+      ansatte: mockAnsatte,
+      setAnsatte: mockSetAnsatte
+    });
+
+    render(<Refusjon id="ansatt1" />);
+
+    const inputFelt = screen.getByRole('textbox');
+
+    fireEvent.change(inputFelt, { target: { value: "1 234 567" } });
+
+    expect(screen.getByText(/Beløpet er for høyt/)).toBeInTheDocument();
+    expect(mockSetAnsatte).toHaveBeenCalledWith(expected)
+  })
+})
--- src/components/ansatte/Refusjon.tsx
+++ src/components/ansatte/Refusjon.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "nav-frontend-skjema";
 import { useAppStore } from "../../data/store/AppStore";
 import { AnsattID } from "../../data/types/sporenstreksTypes";
@@ -7,29 +7,84 @@ import { HjelpetekstRefusjon } from "./HjelpetekstRefusjon";
 import { validateMaksBeloep } from '../validering/validateMaksBeloep';
 
 export const Refusjon = (props: AnsattID) => {
-  const {ansatte, setAnsatte} = useAppStore();
+  const { ansatte, setAnsatte } = useAppStore();
   const a = ansatte.find(a => a.id === props.id);
-  const handleChange = (evt) => {
+  const [value, updateValue] = useState('');
+  var currentVal = 0;
+
+  const getSpecialCharsOnSides = (x: string, cursorPosition: number) => {
+    const specialCharsLeft = x.substring(0, cursorPosition).replace(/[0-9]/g, '').length;
+    const specialCharsRight = x.substring(cursorPosition).replace(/[0-9]/g, '').length;
+    return [specialCharsLeft, specialCharsRight]
+  }
+
+  const cleanNumber = (strNumber: string): string => {
+    const val = strNumber.replace(/,/g, ".");
+    return val.replace(/\s/g, "");
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    let caret: number = event && event.target ? Number(event.target.selectionStart) : 0;
+    const element = event.target
+    window.requestAnimationFrame(() => {
+      element.selectionStart = caret
+      element.selectionEnd = caret
+    })
+
+    // -- Stop cursor jumping when formatting number in React
+    const specialCharsBefore = getSpecialCharsOnSides(event.target.value, caret);
+    let val = cleanNumber(event.target.value);
+
+    let x = Number(val);
+
+    if (isNaN(x)) {
+      val = cleanNumber(value);
+      x = Number(val);
+    }
+
+    let specialCharsAfter;
+    if (currentVal != x) {
+      var formatter = new Intl.NumberFormat("nb-NO", { minimumFractionDigits: 2 });
+      const strCurrentVal = formatter.format(x);
+      updateValue(strCurrentVal);
+      specialCharsAfter = getSpecialCharsOnSides(strCurrentVal, caret);
+    } else {
+      updateValue(val);
+      specialCharsAfter = getSpecialCharsOnSides(val, caret);
+    }
+
+    if (specialCharsBefore[0] < specialCharsAfter[0]) {
+      caret += specialCharsAfter[0] - specialCharsBefore[0];
+    } else if (specialCharsBefore[0] > specialCharsAfter[0]) {
+      caret -= specialCharsBefore[0] - specialCharsAfter[0];
+    } else if (specialCharsBefore[1] > specialCharsAfter[1]) {
+      caret -= specialCharsBefore[1] - specialCharsAfter[1];
+    }
+
     if (a) {
-      const s = filterStringToNumbersOnly(evt.target.value ? evt.target.value : "", 20);
-      a.beloep = s.length === 0 ? undefined : parseInt(evt.target.value);
+      const s = filterStringToNumbersOnly(val, 20);
+      a.beloep = s.length === 0 ? undefined : parseInt(val);
       a.beloepError = validateMaksBeloep(a.beloep);
     }
     setAnsatte([...ansatte]);
   };
+
   return (
     <div>
       <Input
         feil={a?.beloepError}
-        value={a?.beloep ? a?.beloep : ''}
+        value={value}
         bredde={"S"}
         label={
-          <div style={{display: 'flex'}}>
+          <div style={{ display: 'flex' }}>
             Beløp:
-            <HjelpetekstRefusjon/>
+            <HjelpetekstRefusjon />
           </div>}
         placeholder="Beløp"
-        inputMode={"numeric"}
-        onChange={handleChange}/>
+        inputMode={"decimal"}
+        onChange={handleChange} />
     </div>)
 };
+
+export default Refusjon;
