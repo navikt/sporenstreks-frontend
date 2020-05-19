a85e86cade6272a7806d21783f02f1e326c84571
Morten Laukvik <morten.laukvik@nav.no>
2020-05-19 13:31:48 +0200

    Integrasjonstester - skall

---
+++ src/App.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+describe('App', () => {
+
+  it('skal vise default', () => {
+  })
+
+  it('skal vise Sykepenger', () => {
+  })
+  it('skal vise SykepengerBulk', () => {
+  })
+  it('skal vise ExcelOpplasting', () => {
+  })
+
+
+  it('skal vise Kvittering', () => {
+  })
+
+  it('skal vise KvitteringExcel', () => {
+  })
+
+  it('skal vise KvitteringBulk', () => {
+  })
+
+  it('skal vise LoginFornyet', () => {
+  })
+
+});
