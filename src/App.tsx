4a78e505c719cf6c908bd28528cbb743abdfa59d
Morten Laukvik <laukvik@users.noreply.github.com>
2020-05-20 13:53:18 +0200

    Fjern ubrukt kode og litt refaktorering (#92)

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,19 @@ import i18n from './locales/i18n';
 import Sykepenger from './pages/Sykepenger';
 import { Switch, Route } from 'react-router-dom';
 import Redirecter from './components/Redirecter';
-import { DataFetcher } from './data/DataFetcher';
+import { ArbeidsgiverProvider } from './components/arbeidsgiver/ArbeidsgiverProvider';
 import StoreProvider from './data/store/StoreProvider';
-import { Amplitude } from './components/amplitude/AmplitudeProvider';
 import Kvittering from './pages/Kvittering';
 import SykepengerBulk from './pages/SykepengerBulk';
-import ExcelOpplasting from "./pages/ExcelOpplasting";
-import KvitteringExcel from "./pages/KvitteringExcel";
-import KvitteringBulk from "./pages/KvitteringBulk";
+import ExcelOpplasting from './pages/ExcelOpplasting';
+import KvitteringExcel from './pages/KvitteringExcel';
+import KvitteringBulk from './pages/KvitteringBulk';
 import LoginFornyet from './components/loginFornyet/LoginFornyet';
 
 const App = () => {
   return (
     <StoreProvider>
-      <DataFetcher>
-        <Amplitude>
+      <ArbeidsgiverProvider>
           <I18nextProvider i18n={i18n}>
             <Switch>
               <Route path="/enkel" render={() => <Sykepenger />} />
@@ -31,8 +29,7 @@ const App = () => {
               <Route path="/" render={() => <Redirecter />} />
             </Switch>
           </I18nextProvider>
-        </Amplitude>
-      </DataFetcher>
+      </ArbeidsgiverProvider>
     </StoreProvider>
   );
 };
