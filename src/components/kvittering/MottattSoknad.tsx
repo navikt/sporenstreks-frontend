bc97cc5ecc311b5ccd48e1d16286b3c532b008fd
jafeld <jeppe.feld@nav.no>
2020-05-22 14:55:52 +0200

    Refactor kvitteringbulk to use new page (#90)

--- src/components/kvittering/MottattSoknad.tsx
+++ src/components/kvittering/MottattSoknad.tsx
@@ -1,56 +1,59 @@
 import React from 'react';
-import {AlertStripeInfo} from 'nav-frontend-alertstriper';
-import {Innholdstittel, Normaltekst} from 'nav-frontend-typografi';
+import { AlertStripeInfo } from 'nav-frontend-alertstriper';
+import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
 import Panel from 'nav-frontend-paneler';
 import Lenke from "nav-frontend-lenker";
-import {Row} from "nav-frontend-grid";
+import { Row } from "nav-frontend-grid";
+import InnloggetSide from '../../pages/InnloggetSide';
 
 interface MottattSoknadProps {
   nySoknadLink: string,
 }
 
 export const MottattSoknad = ({ nySoknadLink } : MottattSoknadProps) => (
-  <Row>
-    <Panel>
-      <Innholdstittel>Søknaden er mottatt.</Innholdstittel>
-    </Panel>
-    <Panel>
-      <Normaltekst>
-        Trenger du å kontakte oss, er det tilstrekkelig å oppgi fødselsnummeret til den ansatte.
-      </Normaltekst>
-    </Panel>
-    <Panel>
-      <Normaltekst>
-        <Lenke
-          href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/aa-registeret-og-a-meldingen/relatert-informasjon/bankkontonummer-refusjoner-fra-nav-til-arbeidsgiver">
-          Har du registrert kontonummer hos NAV?</Lenke>
-        <span> Hvis ikke må du gjøre det snarest.</span>
-      </Normaltekst>
-    </Panel>
-    <Panel>
-      <AlertStripeInfo>
-        Ønsker dere hyppigere utbetalinger enn vanlig? &nbsp;
-        <Lenke
-          href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546"
-          className="lenke ">I Altinn kan dere endre utbetalingsintervallet.</Lenke>
-      </AlertStripeInfo>
-    </Panel>
-    <Panel>
-      <Normaltekst>
-        <Lenke id="logout"
-               href="https://loginservice.nav.no/slo">Logg ut</Lenke>
-      </Normaltekst>
-      <Normaltekst>
-        <Lenke href={nySoknadLink}>
-          Opprett en ny søknad
-        </Lenke>
-      </Normaltekst>
-      <Normaltekst>
-        <Lenke href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver/"
-               className="lenke informasjonsboks__lenke">
-          Tilbake til Min side - arbeidsgiver
-        </Lenke>
-      </Normaltekst>
-    </Panel>
-  </Row>
+  <InnloggetSide>
+    <Row>
+      <Panel>
+        <Innholdstittel>Søknaden er mottatt.</Innholdstittel>
+      </Panel>
+      <Panel>
+        <Normaltekst>
+          Trenger du å kontakte oss, er det tilstrekkelig å oppgi fødselsnummeret til den ansatte.
+        </Normaltekst>
+      </Panel>
+      <Panel>
+        <Normaltekst>
+          <Lenke
+            href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/aa-registeret-og-a-meldingen/relatert-informasjon/bankkontonummer-refusjoner-fra-nav-til-arbeidsgiver">
+            Har du registrert kontonummer hos NAV?</Lenke>
+          <span> Hvis ikke må du gjøre det snarest.</span>
+        </Normaltekst>
+      </Panel>
+      <Panel>
+        <AlertStripeInfo>
+          Ønsker dere hyppigere utbetalinger enn vanlig? &nbsp;
+          <Lenke
+            href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546"
+            className="lenke ">I Altinn kan dere endre utbetalingsintervallet.</Lenke>
+        </AlertStripeInfo>
+      </Panel>
+      <Panel>
+        <Normaltekst>
+          <Lenke id="logout"
+                 href="https://loginservice.nav.no/slo">Logg ut</Lenke>
+        </Normaltekst>
+        <Normaltekst>
+          <Lenke href={nySoknadLink}>
+            Opprett en ny søknad
+          </Lenke>
+        </Normaltekst>
+        <Normaltekst>
+          <Lenke href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver/"
+                 className="lenke informasjonsboks__lenke">
+            Tilbake til Min side - arbeidsgiver
+          </Lenke>
+        </Normaltekst>
+      </Panel>
+    </Row>
+  </InnloggetSide>
 )
--- src/pages/Kvittering.tsx
+++ src/pages/Kvittering.tsx
@@ -1,49 +1,6 @@
 import React from 'react';
-import {AlertStripeInfo} from 'nav-frontend-alertstriper';
-import {Innholdstittel, Normaltekst} from 'nav-frontend-typografi';
-import Panel from 'nav-frontend-paneler';
-import {Link} from 'react-router-dom';
-import Lenke from "nav-frontend-lenker";
+import { MottattSoknad } from "../components/kvittering/MottattSoknad";
 
-const Kvittering = () => {
-    return (
-        <div className="kvittering">
-            <div className="limit">
-                <Panel>
-                    <div>
-                        <Innholdstittel>Søknaden er mottatt.</Innholdstittel>
-                    </div>
-                  <div style={{margin: "1rem 0rem"}}>
-                    <Normaltekst>
-                      Trenger du å kontakte oss, er det tilstrekkelig å oppgi fødselsnummeret til den ansatte.
-                    </Normaltekst>
-                  </div>
-                  <div style={{marginBottom: "3rem"}}>
-                    <Normaltekst>
-                      <Lenke
-                        href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/aa-registeret-og-a-meldingen/relatert-informasjon/bankkontonummer-refusjoner-fra-nav-til-arbeidsgiver">
-                        Har du registrert kontonummer hos NAV?</Lenke>
-                      <span> Hvis ikke må du gjøre det snarest.</span>
-                    </Normaltekst>
-                  </div>
-                    <AlertStripeInfo>
-                        Ønsker dere hyppigere utbetalinger enn vanlig? &nbsp;
-                        <a href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546" className="lenke ">I Altinn kan dere endre utbetalingsintervallet.</a>
-                    </AlertStripeInfo>
-                    <div><a id="logout"
-                            href="https://loginservice.nav.no/slo" className="lenke informasjonsboks__lenke">Logg ut</a>
-                    </div>
-                    <Link to="/" className="lenke informasjonsboks__lenke">
-                        Opprett en ny søknad
-                    </Link>
-                    <div><a href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver/"
-                            className="lenke informasjonsboks__lenke">
-                        Tilbake til Min side - arbeidsgiver
-                    </a></div>
-                </Panel>
-            </div>
-        </div>
-    );
-};
+const Kvittering = () => <MottattSoknad nySoknadLink="/nettrefusjon/enkel"/>
 
 export default Kvittering;
