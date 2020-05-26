20c84ff002ce7cb751783bb8e3ba0d41ca7802b4
Kent Fjellstad <kent@fjellstad.name>
2020-05-26 11:39:56 +0200

    En test til

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -15,6 +15,7 @@ describe('App', () => {
 
     cleanup()
   })
+  // Dette er testen vi burde ha som mål å få til å virke!
   // it('should have no a11y violations', async () => {
   //   render(<App/>, document.body);
   //   const results = await axe(document.body);
