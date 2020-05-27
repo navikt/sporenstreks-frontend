037e4e15f3be1171c5fa4f3836896dc9d7b66856
Kent Fjellstad <kent@fjellstad.name>
2020-05-27 14:00:10 +0200

    Lagt til landmarks

--- src/components/kvittering/MottattSoknad.tsx
+++ src/components/kvittering/MottattSoknad.tsx
@@ -11,50 +11,53 @@ interface MottattSoknadProps {
   nySoknadLink: string,
 }
 
-export const MottattSoknad = ({ nySoknadLink } : MottattSoknadProps) => (
+export const MottattSoknad = ({ nySoknadLink }: MottattSoknadProps) => (
   <InnloggetSide>
-    <Row>
-      <Panel>
-        <Innholdstittel>Søknaden er mottatt.</Innholdstittel>
-      </Panel>
-      <Panel>
-        <Normaltekst>
-          Trenger du å kontakte oss, er det tilstrekkelig å oppgi fødselsnummeret til den ansatte.
+    <main>
+      <Row>
+        <Panel>
+          <Innholdstittel>Søknaden er mottatt.</Innholdstittel>
+        </Panel>
+        <Panel>
+          <Normaltekst>
+            Trenger du å kontakte oss, er det tilstrekkelig å oppgi fødselsnummeret til den ansatte.
         </Normaltekst>
-      </Panel>
-      <Panel>
-        <Normaltekst>
+        </Panel>
+        <Panel>
+          <Normaltekst>
+            <Lenke
+              href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/aa-registeret-og-a-meldingen/relatert-informasjon/bankkontonummer-refusjoner-fra-nav-til-arbeidsgiver">
+              Har du registrert kontonummer hos NAV?</Lenke>
+            <span> Hvis ikke må du gjøre det snarest.</span>
+          </Normaltekst>
+        </Panel>
+        <Panel>
+          <AlertStripeInfo>
+            Ønsker dere hyppigere utbetalinger enn vanlig? &nbsp;
           <Lenke
-            href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/aa-registeret-og-a-meldingen/relatert-informasjon/bankkontonummer-refusjoner-fra-nav-til-arbeidsgiver">
-            Har du registrert kontonummer hos NAV?</Lenke>
-          <span> Hvis ikke må du gjøre det snarest.</span>
-        </Normaltekst>
-      </Panel>
-      <Panel>
-        <AlertStripeInfo>
-          Ønsker dere hyppigere utbetalinger enn vanlig? &nbsp;
-          <Lenke
-            href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546"
-            className="lenke ">I Altinn kan dere endre utbetalingsintervallet.</Lenke>
-        </AlertStripeInfo>
-      </Panel>
-      <Panel>
-        <Normaltekst>
-          <Lenke id="logout"
-                 href="https://loginservice.nav.no/slo">Logg ut</Lenke>
-        </Normaltekst>
-        <Normaltekst>
-          <InternLenke to={nySoknadLink}>
-            Opprett en ny søknad
-          </InternLenke>
-        </Normaltekst>
-        <Normaltekst>
-          <Lenke href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver/"
-                 className="lenke informasjonsboks__lenke">
-            Tilbake til Min side - arbeidsgiver
+              href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546">
+              I Altinn kan dere endre utbetalingsintervallet.
           </Lenke>
-        </Normaltekst>
-      </Panel>
-    </Row>
+          </AlertStripeInfo>
+        </Panel>
+        <Panel>
+          <Normaltekst>
+            <Lenke id="logout"
+              href="https://loginservice.nav.no/slo">Logg ut</Lenke>
+          </Normaltekst>
+          <Normaltekst>
+            <InternLenke to={nySoknadLink}>
+              Opprett en ny søknad
+          </InternLenke>
+          </Normaltekst>
+          <Normaltekst>
+            <Lenke href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver/"
+              className="informasjonsboks__lenke">
+              Tilbake til Min side - arbeidsgiver
+          </Lenke>
+          </Normaltekst>
+        </Panel>
+      </Row>
+    </main>
   </InnloggetSide>
 )
