36425833b94d4ecfdde758edb0ca9bf79c766292
Kent Fjellstad <kent@fjellstad.name>
2020-06-05 10:26:31 +0200

    Refaktorering av KnappMedVarsel for å få den til å virke med IE11

--- src/components/KnappMedVarsel.tsx
+++ src/components/KnappMedVarsel.tsx
@@ -12,13 +12,11 @@ const KnappMedVarsel = ({ disabledClick, disabled, children }: KnappMedVarselInt
   return (
     <>
       {(disabled) ?
-        <button className={'knapp-med-varsel-disabled-button'} onClick={e => disabledClick(e)} onKeyDown={e => disabledClick(e)}>
-          <Hovedknapp disabled={disabled} className="knapp knapp-med-varsel-action">
-            {children}
-          </Hovedknapp>
+        <button className={'knapp-med-varsel-action knapp--disabled knapp-med-varsel-disabled-button'} onClick={e => disabledClick(e)} onKeyDown={e => disabledClick(e)}>
+          {children}
         </button>
         :
-        <Hovedknapp disabled={disabled} className="knapp knapp-med-varsel-action">
+        <Hovedknapp className="knapp knapp-med-varsel-action">
           {children}
         </Hovedknapp>
       }
