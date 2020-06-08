b6444c4e4c642160a5c1c19ac84924d6a3405a59
Morten Laukvik <morten.laukvik@nav.no>
2020-06-08 13:45:50 +0200

    Legg til resten av metodene til excel skjema også

--- src/context/ExcelSkjemaContext.tsx
+++ src/context/ExcelSkjemaContext.tsx
@@ -1,10 +1,21 @@
 import React, { createContext, useContext, useState } from 'react';
+import { tabellFeil } from '../components/feilvisning/FeilTabell';
 
-export const buildExcelSkjemaContext = () => ({
-
+export const buildExcelSkjemaContext = (fileName: string = '', visAlleFeil: boolean = false, hasTriedSubmit: boolean = false,
+                                        feil: tabellFeil[] = [], file?: File ) => ({
+  fileName,
+  setFileName: function(filename: string){},
+  file,
+  setFile: function(file: File){},
+  visAlleFeil,
+  setVisAlleFeil: function(visAlleFeil:boolean){},
+  hasTriedSubmit,
+  setHasTriedSubmit: function(hasTriedSubmit:boolean){},
+  feil,
+  setFeil: function(feil: tabellFeil[]){}
 })
 
-const ExcelSkjemaContext = createContext(buildExcelSkjemaContext());
+const ExcelSkjemaContext = createContext(buildExcelSkjemaContext('', false, false, []));
 
 interface ArbeidsgiverContextProviderProps {
   children: any
@@ -13,8 +24,15 @@ interface ArbeidsgiverContextProviderProps {
 export const useExcelSkjema = () => useContext(ExcelSkjemaContext);
 
 export const ExcelSkjemaProvider = (props: ArbeidsgiverContextProviderProps) => {
+  const [fileName, setFileName] = useState('Last opp utfylt Excel-mal');
+  const [file, setFile] = useState<File>();
+  const [feil, setFeil] = useState<tabellFeil[]>([]);
+  const [visAlleFeil, setVisAlleFeil] = useState(false)
+  const [hasTriedSubmit, setHasTriedSubmit] = useState(false)
+
   return (
-    <ExcelSkjemaContext.Provider value={{  }}>
+    <ExcelSkjemaContext.Provider value={{ fileName, setFileName, file, setFile, feil, setFeil,
+      visAlleFeil, setVisAlleFeil, hasTriedSubmit, setHasTriedSubmit }}>
       { props.children }
     </ExcelSkjemaContext.Provider>
   )
