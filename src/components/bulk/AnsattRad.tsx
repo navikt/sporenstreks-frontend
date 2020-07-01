2fcc356ba15ad3489450e929a4233978ff8e7d41
Kent Fjellstad <kent@fjellstad.name>
2020-07-01 17:13:19 +0200

    Typoish

--- src/components/bulk/AnsattRad.tsx
+++ src/components/bulk/AnsattRad.tsx
@@ -23,7 +23,7 @@ export const AnsattRad = ({ id }: AnsattID) => {
   const a = ansatte.find(a => a.id === id) || byggAnsatt()
   if (a.status === SkjemaStatus.GODKJENT) {
     return (
-      <Row key={a?.id} className="AnsattRad">
+      <Row key={a.id} className="AnsattRad">
         <Column md="1" xs="12"><RadNr nr={ansatte.indexOf(a) + 1}/></Column>
         <Column md="2" xs="12">
           <BulkVisning label="Fødselsnummer:">
