23ae3be839f63f5febacb6a7f4311d5faea6b5e8
Kent Fjellstad <Kent@fjellstad.name>
2020-09-10 10:51:22 +0200

    Bulk tester (#186)
    * Tester at man har erklært før man kan sende inn
    
    * Tester på det som er igjen av kode

--- src/components/bulk/Ansatte.test.tsx
+++ src/components/bulk/Ansatte.test.tsx
@@ -7,6 +7,16 @@ import React from 'react';
 import { BulkProvider } from '../../context/BulkContext';
 import { MemoryRouter } from 'react-router-dom';
 import { BackendResponseState, BackendStatus, SkjemaStatus } from '../../data/types/sporenstreksTypes';
+import { Linker } from '../../pages/Linker';
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
 describe('Ansatte', () => {
 
@@ -193,6 +203,71 @@ describe('Ansatte', () => {
     expect(screen.queryAllByText('Du er blitt logget ut, følg instruksjonene for ikke å miste data').length).toEqual(0)
   })
 
+  it('viser ikke feilmelding om man klikker send inn etter at man har huket av erklæringen', () => {
+    render(
+      <AppStoreProvider tokenExpired={false}>
+        <ArbeidsgiverProvider arbeidsgivere={mockArbeidsgiverValues} status={Status.Successfully}>
+          <MemoryRouter initialEntries={['/']}>
+            <BulkProvider ansatte={ansatte} feil={[]}>
+              <Ansatte />
+            </BulkProvider>
+          </MemoryRouter>
+        </ArbeidsgiverProvider>
+      </AppStoreProvider>
+    );
+
+    const checkbox = screen.getByLabelText('Vi erklærer:');
+    fireEvent.click(checkbox);
+
+    const submitButton = screen.getByText('Send søknad om refusjon');
+    fireEvent.click(submitButton);
+
+    expect(screen.queryAllByText(/krysse av avkrysningsboksen over send-knappen/).length).toEqual(0)
+  })
+
+  it('viser feilmelding om man klikker send inn uten å ha sjekket av at man erklærer', () => {
+    render(
+      <AppStoreProvider tokenExpired={false}>
+        <ArbeidsgiverProvider arbeidsgivere={mockArbeidsgiverValues} status={Status.Successfully}>
+          <MemoryRouter initialEntries={['/']}>
+            <BulkProvider ansatte={ansatte} feil={[]}>
+              <Ansatte />
+            </BulkProvider>
+          </MemoryRouter>
+        </ArbeidsgiverProvider>
+      </AppStoreProvider>
+    );
+
+    const submitButton = screen.getByText('Send søknad om refusjon');
+    fireEvent.click(submitButton);
+
+    expect(screen.queryAllByText(/krysse av avkrysningsboksen over send-knappen/).length).toEqual(1)
+  })
+
+  it('skjer ingenting om man gjør en submit på formen', () => {
+    let { asFragment } = render(
+      <AppStoreProvider tokenExpired={false}>
+        <ArbeidsgiverProvider arbeidsgivere={mockArbeidsgiverValues} status={Status.Successfully}>
+          <MemoryRouter initialEntries={['/']}>
+            <BulkProvider ansatte={ansatte} feil={[]}>
+              <Ansatte />
+            </BulkProvider>
+          </MemoryRouter>
+        </ArbeidsgiverProvider>
+      </AppStoreProvider>
+    );
+
+    const the_form = screen.getByTestId('bulk-form');
+
+    const firstRender = asFragment();
+
+    fireEvent.submit(the_form);
+
+    const secondRender = asFragment();
+
+    expect(firstRender).toEqual(secondRender);
+  })
+
   it('viser valideringsfeil fra backend', async () => {
     const backendResponce: BackendStatus[] = [
       {
@@ -248,6 +323,56 @@ describe('Ansatte', () => {
 
     expect(screen.getByText('Det er feil i skjemaet'))
     expect(screen.getByText(/Det er en feil med identitsnummer/))
+  })
+
+
+  it('sender brukeren til kvitteringsiden når alt har gått bra i backend, dvs. vi har referansenummer på alle innsendingene', async () => {
+    const backendResponce: BackendStatus[] = [
+      {
+        status: BackendResponseState.OK,
+        validationErrors: null,
+        genericMessage: null,
+        referenceNumber: '1234'
+      },
+      {
+        status: BackendResponseState.OK,
+        validationErrors: null,
+        genericMessage: null,
+        referenceNumber: '2345'
+      },
+    ]
+
+    jest.spyOn(global, 'fetch').mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(backendResponce)
+      })
+    );
+
+    render(
+      <AppStoreProvider tokenExpired={false}>
+        <ArbeidsgiverProvider arbeidsgivere={mockArbeidsgiverValues} status={Status.Successfully}>
+          <MemoryRouter initialEntries={['/']}>
+            <BulkProvider ansatte={ansatte} feil={[]}>
+              <Ansatte />
+            </BulkProvider>
+          </MemoryRouter>
+        </ArbeidsgiverProvider>
+      </AppStoreProvider>
+    );
+
+    const erklarerCheck = screen.getByText('Vi erklærer:');
+    fireEvent.click(erklarerCheck);
+
+    const button = screen.getByText(/Send søknad om refusjon/);
+    fireEvent.click(button);
+
+    await act(async () => {
+      const buttons = screen.getAllByText(/Send søknad om refusjon/);
+      fireEvent.click(buttons[1]);
+    });
+
+    expect(mockHistoryPush).toHaveBeenCalledWith(Linker.BulkKvittering);
   })
 
   it('skal vise at du har blitt logget av ved 401', async () => {
