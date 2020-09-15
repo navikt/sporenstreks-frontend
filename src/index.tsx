58652902bc95e80e5e1d66724f36a9acb8246444
Kent Fjellstad <kent.fjellstad@nav.no>
2020-09-15 07:45:54 +0200

    La til sentry

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import './index.css';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
+
+Sentry.init({dsn: "https://3769b2c742c840f085ca72d93f49bb0e@sentry.gc.nav.no/39"});
 
 ReactDOM.render(
   <BrowserRouter basename="nettrefusjon">
