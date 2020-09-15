1de37e40cb4d69d54375123603e9b12f55e81c7d
tuxbear <ole-andre.riga.johansen@nav.no>
2020-09-15 13:32:40 +0200

    Sonar Qube reporting on PRs

---
+++ run-sonar.js
@@ -0,0 +1,15 @@
+const scanner = require('sonarqube-scanner');
+
+scanner(
+    {
+        serverUrl : 'https://sonarcloud.io',
+        token : process.env.SONAR_TOKEN,
+        options: {
+            'sonar.organization': 'navikt',
+            'sonar.sonar.projectKey': 'navikt_sporenstreks-frontend',
+            'sonar.sources': 'src',
+            'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info'
+        }
+    },
+    () => process.exit()
+)
