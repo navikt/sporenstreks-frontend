b0535da816a8701730ddc4416f1a71e4b8231cab
Kent Fjellstad <Kent@fjellstad.name>
2020-09-30 08:43:00 +0200

    Fjerner tekst om hyppigere utbetalinger (#198)
    * Fjerner tekst om hyppigere utbetalinger

--- src/components/felles/MottattSoknad.tsx
+++ src/components/felles/MottattSoknad.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { AlertStripeInfo } from 'nav-frontend-alertstriper';
 import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
 import Panel from 'nav-frontend-paneler';
 import Lenke from 'nav-frontend-lenker';
@@ -35,15 +34,6 @@ export const MottattSoknad = ({ nySoknadLink }: MottattSoknadProps) => (
               Har du registrert kontonummer hos NAV?</Lenke>
             <span> Hvis ikke må du gjøre det snarest.</span>
           </Normaltekst>
-        </Panel>
-        <Panel>
-          <AlertStripeInfo>
-            Ønsker dere hyppigere utbetalinger enn vanlig? &nbsp;
-          <Lenke
-              href=" https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=1&ServiceCode=5546">
-              I Altinn kan dere endre utbetalingsintervallet.
-          </Lenke>
-          </AlertStripeInfo>
         </Panel>
         <Panel>
           <Normaltekst>
