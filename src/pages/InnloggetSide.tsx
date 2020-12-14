04e899d0660b9e95a295bd793da4f2dc46013b2a
Kent Fjellstad <kent.fjellstad@nav.no>
2020-12-14 15:29:32 +0100

    Skiplink

--- src/pages/InnloggetSide.tsx
+++ src/pages/InnloggetSide.tsx
@@ -13,8 +13,8 @@ import { useArbeidsgiver } from '../context/ArbeidsgiverContext';
 import TimeoutAdvarsel from '../components/login/TimeoutAdvarsel';
 
 interface SideProps {
-  children: React.ReactNode,
-  className?: string
+  children: React.ReactNode;
+  className?: string;
 }
 
 const InnloggetSide = (props: SideProps) => {
@@ -22,48 +22,51 @@ const InnloggetSide = (props: SideProps) => {
   const { t } = useTranslation();
   const history: History = useHistory();
   return (
-    <main className={'innloggetside ' + props.className}>
-        {arbeidsgivere.length === 0 &&
-          <AlertStripeAdvarsel>
-            <p>Du har ikke rettigheter til å søke om refusjon for noen bedrifter</p>
-            <p>Tildeling av roller foregår i Altinn</p>
-            <Link to="/min-side-arbeidsgiver/informasjon-om-tilgangsstyring">
-              Les mer om roller og tilganger.
-            </Link>
-          </AlertStripeAdvarsel>
-        }
-        {arbeidsgivere.length > 0 &&
-          <>
-            <Bedriftsmeny
-              history={history}
-              onOrganisasjonChange={(org: Organisasjon) => {
-                setArbeidsgiverId(org.OrganizationNumber);
-                setFirma(org.Name);
-              }}
-              sidetittel={t(Keys.MY_PAGE)}
-              organisasjoner={arbeidsgivere}
-            />
+    <main className={'innloggetside ' + props.className} id='maincontent'>
+      {arbeidsgivere.length === 0 && (
+        <AlertStripeAdvarsel>
+          <p>
+            Du har ikke rettigheter til å søke om refusjon for noen bedrifter
+          </p>
+          <p>Tildeling av roller foregår i Altinn</p>
+          <Link to='/min-side-arbeidsgiver/informasjon-om-tilgangsstyring'>
+            Les mer om roller og tilganger.
+          </Link>
+        </AlertStripeAdvarsel>
+      )}
+      {arbeidsgivere.length > 0 && (
+        <>
+          <Bedriftsmeny
+            history={history}
+            onOrganisasjonChange={(org: Organisasjon) => {
+              setArbeidsgiverId(org.OrganizationNumber);
+              setFirma(org.Name);
+            }}
+            sidetittel={t(Keys.MY_PAGE)}
+            organisasjoner={arbeidsgivere}
+          />
 
-            <Container>
-              <Row>
-                <Column>
-                  <div className={'innloggetside__minside_arbeidsgiver'}>
-                    <Lenke href="/min-side-arbeidsgiver/">&lt;&lt; Min side arbeidsgiver</Lenke>
-                  </div>
-                </Column>
-              </Row>
-            </Container>
+          <Container>
+            <Row>
+              <Column>
+                <div className={'innloggetside__minside_arbeidsgiver'}>
+                  <Lenke href='/min-side-arbeidsgiver/'>
+                    &lt;&lt; Min side arbeidsgiver
+                  </Lenke>
+                </div>
+              </Column>
+            </Row>
+          </Container>
 
-            <TimeoutAdvarsel/>
+          <TimeoutAdvarsel />
 
-            <Container className={'innloggetside__innhold'}>
-                { props.children }
-            </Container>
-
-          </>
-        }
+          <Container className={'innloggetside__innhold'}>
+            {props.children}
+          </Container>
+        </>
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default InnloggetSide
+export default InnloggetSide;
