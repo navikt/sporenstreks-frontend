5e80994c3a64e203b3ee3e9ea3c22b536ca8969d
Morten Laukvik <morten.laukvik@nav.no>
2021-09-28 15:55:36 +0200

    Proxy: fikser

--- server/server.js
+++ server/server.js
@@ -21,50 +21,26 @@ const startServer = () => {
     res.redirect('/nettrefusjon/');
   });
 
-  app.get(BASE_PATH + '/api/env', (req, res) => {
-    res.status(200).send({
-      'login-url': process.env.LOGIN_URL,
-      'api-url': process.env.API_BACKEND_URL
-    });
-  });
-
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, HOME_FOLDER, 'index.html'));
-  });
-
-  app.use(function (req, res) {
-    // eslint-disable-next-line no-console
-    console.error('Server: Error 404', req.url);
-    res.status(404).send('404 not found');
-  });
-
-  app.use(function (err, req, res) {
-    // eslint-disable-next-line no-console
-    console.error('Server: Error 500', err);
-    res.status(500).send('500 Error');
-  });
-
-  // API_GATEWAY  = "https://api-gw.oera.no"
-  // APIGW_HEADER =
-  // apiKey       =
-
-  var apiKey = fs.readFileSync('/apigw/sporenstreks/x-nav-apiKey', { encoding: 'utf8', flag: 'r' })
+  var apiKey = fs.readFileSync('/Users/laukvik/apigw/sporenstreks/x-nav-apiKey', { encoding: 'utf8', flag: 'r' })
   console.log("apiKey", apiKey)
 
   const proxyConfig = {
     changeOrigin: true,
-    target: process.env.API_GATEWAY || 'http://localhost:8080',
-    pathRewrite: {
-      '^/api': 'api'
-    },
-    xfwd: true,
+    target: 'http://www.google.no',
+    // pathRewrite: {
+    //   '^/api': 'api'
+    // },
+    // xfwd: true,
     headers: {
       'x-nav-apiKey': apiKey
     }
   };
-
   console.log("proxyConfig", proxyConfig)
   app.use("/api", createProxyMiddleware(proxyConfig));
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, HOME_FOLDER, 'index.html'));
+  });
 
   app.listen(PORT, () => {
     // eslint-disable-next-line no-console
