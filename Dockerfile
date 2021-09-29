8afe3e46b041fed97f801e4ee59c5e2ac742aadb
Morten Laukvik <morten.laukvik@nav.no>
2021-09-29 10:54:01 +0200

    Bruk nginx

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM nginxinc/nginx-unprivileged
 # User env var is needed for luarocks to not complain.
 ENV APP_DIR="/app" \
 	APP_PATH_PREFIX="/nettrefusjon" \
-	USER="root"
+	USER="root" \
+	APP_PORT="8080"
 
 # Copying over the config-files.
 COPY files/default-config.nginx /etc/nginx/conf.d/app.conf.template
@@ -15,6 +16,6 @@ COPY build /app
 
 EXPOSE 8080
 
-WORKDIR ${APP_DIR}
+WORKDIR /app
 
 CMD sh /usr/sbin/start-nginx
--- files/start-nginx.sh
+++ files/start-nginx.sh
@@ -18,9 +18,9 @@ then
 fi
 
 # Setting default environment variables
-export APP_DIR="${APP_DIR:-/app}"
+export APP_DIR="/app"
 export APP_PATH_PREFIX="/nettrefusjon"
-export APP_PORT="${APP_PORT:-443}"
+export APP_PORT="8080"
 export API_GW_API_KEY="${API_GW_API_KEY:-dummykey}"
 export API_GATEWAY="${API_GATEWAY:-http://localhost:8080}"
 export APP_VERSION="${APP_VERSION:-localhost}"
@@ -35,11 +35,13 @@ echo -e "Startup:" ${APP_PATH_PREFIX} \n"\
 envsubst '$API_GW_API_KEY $APP_DIR $APP_PORT $APP_PATH_PREFIX $API_GATEWAY $RESOLVER' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
 # find all env start with APP_
-export SUBS=$(echo $(env | cut -d= -f1 | grep "^APP_" | sed -e 's/^/\$/'))
+#export SUBS=$(echo $(env | cut -d= -f1 | grep "^APP_" | sed -e 's/^/\$/'))
 
 # replace above envs
-echo "Startup inject envs: " ${SUBS}
-for f in `find /${APP_DIR} -regex ".*\.\(js\|css\|html\|json\|map\)"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
-for f in `find /nginx -regex ".*\.nginx"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
-/usr/local/openresty/bin/openresty -g 'daemon off;'
-exec "$@"
+#echo "Startup inject envs: " ${SUBS}
+#for f in `find /${APP_DIR} -regex ".*\.\(js\|css\|html\|json\|map\)"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
+#for f in `find /nginx -regex ".*\.nginx"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
+nginx -g "daemon off;" &
+pid=$!
+wait "$pid"
+
