89afea4e7fc83d9f956320d1912683741bcf2a84
Kent Fjellstad <kent.fjellstad@nav.no>
2021-09-29 08:19:27 +0200

    Uten rewrite

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const fs = require('fs')
+const fs = require('fs');
 
 const BASE_PATH = '/nettrefusjon';
 const HOME_FOLDER = '../build';
@@ -21,22 +21,25 @@ const startServer = () => {
     res.redirect('/nettrefusjon/');
   });
 
-  var apiKey = fs.readFileSync('/apigw/sporenstreks/x-nav-apiKey', { encoding: 'utf8', flag: 'r' })
-  console.log("apiKey", apiKey)
+  var apiKey = fs.readFileSync('/apigw/sporenstreks/x-nav-apiKey', {
+    encoding: 'utf8',
+    flag: 'r'
+  });
+  console.log('apiKey', apiKey);
 
   const proxyConfig = {
     changeOrigin: true,
     target: process.env.API_GATEWAY || 'https://api-gw-q1.oera.no/sporenstreks',
-    pathRewrite: {
-      '^/nettrefusjon/api': 'https://sporenstreks.dev.nav.no/api'
-    },
+    // pathRewrite: {
+    //   '^/nettrefusjon/api': '/api'
+    // },
     // xfwd: true,
     headers: {
       'x-nav-apiKey': apiKey
     }
   };
-  console.log("proxyConfig", proxyConfig)
-  app.use("/api", createProxyMiddleware(proxyConfig));
+  console.log('proxyConfig', proxyConfig);
+  app.use('/api', createProxyMiddleware(proxyConfig));
 
   app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, HOME_FOLDER, 'index.html'));
