ef5a5b78261f2138a3d142e6634aff60538ea320
Morten Laukvik <morten.laukvik@nav.no>
2021-09-29 14:18:35 +0200

    Bruk nginx env

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,12 @@
 FROM nginxinc/nginx-unprivileged
 
-
 # Copying over the config-files.
 COPY files/default-config.nginx /etc/nginx/conf.d/app.conf.template
-# RUN chmod u+x files/start-nginx.sh
 COPY files/start-nginx.sh       /usr/sbin/start-nginx
 
-# RUN mkdir -p /nginx
-COPY build /app
+COPY build /usr/share/nginx/html
 
 EXPOSE 8080
 
-WORKDIR /app
 
 CMD sh /usr/sbin/start-nginx
--- files/start-nginx.sh
+++ files/start-nginx.sh
@@ -32,17 +32,16 @@ echo "APP_DIR='${APP_DIR}'"
 echo "APP_PATH_PREFIX='${APP_PATH_PREFIX}'"
 echo "APP_PORT='${APP_PORT}'"
 
-
 #echo -e "Startup:" ${APP_PATH_PREFIX} \n"
 
 # replace env for nginx conf
-envsubst '$API_GW_API_KEY $APP_DIR $APP_PORT $APP_PATH_PREFIX $API_GATEWAY $RESOLVER' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$API_GW_API_KEY  $APP_PORT $APP_PATH_PREFIX $API_GATEWAY $RESOLVER' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
 # find all env start with APP_
 #export SUBS=$(echo $(env | cut -d= -f1 | grep "^APP_" | sed -e 's/^/\$/'))
 
 # replace above envs
-#echo "Startup inject envs: " ${SUBS}
+echo "Startup inject envs: " ${SUBS}
 #for f in `find /${APP_DIR} -regex ".*\.\(js\|css\|html\|json\|map\)"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
 #for f in `find /nginx -regex ".*\.nginx"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
 nginx -g "daemon off;" &
