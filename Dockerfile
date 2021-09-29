8baa709be0bc20d1c09ecf42d160f47dcb6d035f
Morten Laukvik <morten.laukvik@nav.no>
2021-09-29 15:40:39 +0200

    Bruk nginx env

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
-FROM nginxinc/nginx-unprivileged
 
-# Copying over the config-files.
-COPY files/default-config.nginx /etc/nginx/conf.d/app.conf.template
-COPY files/start-nginx.sh       /usr/sbin/start-nginx
-
-COPY build /usr/share/nginx/html
-
-EXPOSE 8080
+FROM nginxinc/nginx-unprivileged:1.21.3-alpine
 
 
-CMD sh /usr/sbin/start-nginx
+ADD files/proxy.nginx /etc/nginx/conf.d/app.conf.template
+
+ADD files/start-nginx.sh       /start-nginx.sh
+
+ENV APP_DIR="/app" \
+  APP_PATH_PREFIX="/nettrefusjon" \
+  APP_CALLBACK_PATH="/k9/sak/cb" \
+  APP_URL_SAK="http://k9-sak"
+
+COPY build /etc/nginx/html
+
+EXPOSE 9000
+
+CMD sh /start-nginx.sh
--- files/start-nginx.sh
+++ files/start-nginx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # fetching env from vault file.
 if test -d /var/run/secrets/nais.io/vault;
@@ -18,12 +18,19 @@ then
 fi
 
 # Setting default environment variables
-export APP_DIR="/app"
-export APP_PATH_PREFIX="/nettrefusjon"
-export APP_PORT="8080"
-export API_GW_API_KEY="${API_GW_API_KEY:-dummykey}"
-export API_GATEWAY="${API_GATEWAY:-http://localhost:8080}"
-export RESOLVER=$(cat /etc/resolv.conf | grep -v '^#' | grep -m 1 nameserver | awk '{print $2}') # Picking the first nameserver.
+#export APP_DIR="/app"
+#export APP_PATH_PREFIX="/nettrefusjon"
+#export APP_PORT="8080"
+#export API_GW_API_KEY="${API_GW_API_KEY:-dummykey}"
+#export API_GATEWAY="${API_GATEWAY:-http://localhost:8080}"
+#export RESOLVER=$(cat /etc/resolv.conf | grep -v '^#' | grep -m 1 nameserver | awk '{print $2}') # Picking the first nameserver.
+
+export APP_HOSTNAME="${HOSTNAME:-localhost}"
+export APP_PORT="${APP_PORT:-9000}"
+export APP_NAME="${APP_NAME:-devimg}"
+export APP_VERSION="${APP_VERSION:-localhost}"
+export API_GW_API_KEY="tull"
+export API_GATEWAY=""
 
 echo "API Gateway='${API_GATEWAY}'"
 echo "API Gateway key='${API_GW_API_KEY}'"
@@ -35,13 +42,14 @@ echo "APP_PORT='${APP_PORT}'"
 #echo -e "Startup:" ${APP_PATH_PREFIX} \n"
 
 # replace env for nginx conf
-envsubst '$API_GW_API_KEY  $APP_PORT $APP_PATH_PREFIX $API_GATEWAY $RESOLVER' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$APP_NAME $API_GW_API_KEY $APP_VERSION $APP_HOSTNAME $APP_PORT $APP_PATH_PREFIX $API_GATEWAY $RESOLVER' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
-# find all env start with APP_
-#export SUBS=$(echo $(env | cut -d= -f1 | grep "^APP_" | sed -e 's/^/\$/'))
+echo "### Nginx conf ###"
+cat /etc/nginx/conf.d/default.conf
+echo
 
 # replace above envs
-echo "Startup inject envs: " ${SUBS}
+#echo "Startup inject envs: " ${SUBS}
 #for f in `find /${APP_DIR} -regex ".*\.\(js\|css\|html\|json\|map\)"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
 #for f in `find /nginx -regex ".*\.nginx"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
 nginx -g "daemon off;" &
