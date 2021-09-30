6c302e0e4ac63dc1d4412b5ec4bfc90d3bb62b70
Morten Laukvik <morten.laukvik@nav.no>
2021-09-30 08:57:41 +0200

    Nginx: envsubst

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM nginxinc/nginx-unprivileged:1.21.3-alpine
 
-#ADD nginx/config.nginx /etc/nginx/conf.d/app.conf.template
-ADD nginx/config.nginx /etc/nginx/conf.d/default.conf
+ADD nginx/config.nginx /etc/nginx/conf.d/sporenstreks.conf.template
 ADD nginx/start.sh       /start.sh
 
 COPY build /etc/nginx/html
--- nginx/start.sh
+++ nginx/start.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 set -e
 
+echo "### Vault config ###"
+if test -d /var/run/secrets/nais.io/vault;
+then
+    for FILE in /var/run/secrets/nais.io/vault/*.env
+    do
+        for line in $(cat ${FILE}); do
+            echo "Startup: exporting `echo ${line} | cut -d '=' -f 1`"
+            export ${line}
+        done
+    done
+fi
+
+
+echo "### API Gateway ###"
+export APIGW_API_KEY="emptyApiKey"
+if test -d /apigw/sporenstreks;
+then
+  export APIGW_API_KEY=$(cat /apigw/sporenstreks/x-nav-apiKey)
+fi
+# API_GATEWAY må komme inn som env variabel
+export API_GATEWAY="${API_GATEWAY:-http://localhost}"
+
+echo "### Updating ###"
+envsubst '$APIGW_API_KEY  $API_GATEWAY' < /etc/nginx/conf.d/sporenstreks.conf.template > /etc/nginx/conf.d/default.conf
+
+
 echo "### Nginx conf ###"
 cat /etc/nginx/conf.d/default.conf
 echo
-
-export API_GW_API_KEY=""
 
 nginx -g "daemon off;" &
 pid=$!
