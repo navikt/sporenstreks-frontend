a9a6f14a0c6eb09f4015e61bbb26cd9e5dac78a9
Morten Laukvik <morten.laukvik@nav.no>
2021-09-30 09:20:11 +0200

    Nginx: ikke let i vault

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-echo "### Vault config ###"
-if test -d /var/run/secrets/nais.io/vault;
-then
-    for FILE in /var/run/secrets/nais.io/vault/*.env
-    do
-        for line in $(cat ${FILE}); do
-            echo "Startup: exporting `echo ${line} | cut -d '=' -f 1`"
-            export ${line}
-        done
-    done
-fi
+#echo "### Vault - exporting variabels as environment variables ###"
+#if test -d /var/run/secrets/nais.io/vault;
+#then
+#    for FILE in /var/run/secrets/nais.io/vault/*.env
+#    do
+#        for line in $(cat ${FILE}); do
+#            echo "Startup: exporting `echo ${line} | cut -d '=' -f 1`"
+#            export ${line}
+#        done
+#    done
+#fi
 
 
 echo "### API Gateway ###"
@@ -23,11 +23,16 @@ fi
 # API_GATEWAY må komme inn som env variabel
 export API_GATEWAY="${API_GATEWAY:-http://localhost}"
 
-echo "### Updating ###"
+#      - kvPath: apikey/apigw/dev/sporenstreks/sporenstreks-frontend_q1
+#        mountPath: /apigw/sporenstreks
+#By convention blir nøkkelen lagret i vault på stien
+#/apikey/apigw/$ENV/$API/$CONSUMER
+
+echo "### Nginx - replacing variables in config file and creates default.conf ###"
 envsubst '$APIGW_API_KEY  $API_GATEWAY' < /etc/nginx/conf.d/sporenstreks.conf.template > /etc/nginx/conf.d/default.conf
 
 
-echo "### Nginx conf ###"
+echo "### Nginx - displaying the config to use ###"
 cat /etc/nginx/conf.d/default.conf
 echo
 
