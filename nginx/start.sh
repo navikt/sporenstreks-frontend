18d035ea101b81147aaa4c19deb6683c4fca3f3a
Morten Laukvik <morten.laukvik@nav.no>
2021-09-30 09:29:52 +0200

    Nginx: ikke hent api key

--- nginx/start.sh
+++ nginx/start.sh
@@ -16,17 +16,13 @@ set -e
 
 echo "### API Gateway ###"
 export APIGW_API_KEY="emptyApiKey"
-if test -d /apigw/sporenstreks;
-then
-  export APIGW_API_KEY=$(cat /apigw/sporenstreks/x-nav-apiKey)
-fi
+#if test -d /apigw/sporenstreks;
+#then
+#  export APIGW_API_KEY=$(cat /apigw/sporenstreks/x-nav-apiKey)
+#fi
 # API_GATEWAY må komme inn som env variabel
 export API_GATEWAY="${API_GATEWAY:-http://localhost}"
 
-#      - kvPath: apikey/apigw/dev/sporenstreks/sporenstreks-frontend_q1
-#        mountPath: /apigw/sporenstreks
-#By convention blir nøkkelen lagret i vault på stien
-#/apikey/apigw/$ENV/$API/$CONSUMER
 
 echo "### Nginx - replacing variables in config file and creates default.conf ###"
 envsubst '$APIGW_API_KEY  $API_GATEWAY' < /etc/nginx/conf.d/sporenstreks.conf.template > /etc/nginx/conf.d/default.conf
