9a05801b19aa2af5147bb5347a03c02f110a17ea
Kent Fjellstad <kent.fjellstad@nav.no>
2022-01-12 15:29:50 +0100

    Setter defaultverdi på parentOrganizationNumber, siden den ikke kan være NULL

--- src/api/ArbeidsgiverAPI.ts
+++ src/api/ArbeidsgiverAPI.ts
@@ -1,15 +1,6 @@
 import { Organisasjon } from '@navikt/bedriftsmeny/lib/organisasjon';
 import env from '../components/felles/environment';
-
-export const mapArbeidsgiver = (data): Organisasjon[] =>
-  data.map((organisasjon) => ({
-    Name: organisasjon.name,
-    Type: organisasjon.type,
-    OrganizationNumber: organisasjon.organizationNumber,
-    OrganizationForm: organisasjon.organizationForm,
-    Status: organisasjon.status,
-    ParentOrganizationNumber: organisasjon.parentOrganizationNumber
-  }));
+import mapArbeidsgiver from './mapArbeidsgiver';
 
 export interface ArbeidsgivereInterface {
   status: number;
