687d91fcf8e7584350ee3f5671d4ac064fe74384
Kent Fjellstad <kent.fjellstad@nav.no>
2022-01-13 09:14:20 +0100

    Flytter litt på filer

--- src/api/mapArbeidsgiver.test.ts
+++ src/api/mapArbeidsgiver.test.ts
@@ -1,6 +1,6 @@
 import mapArbeidsgiver from './mapArbeidsgiver';
 import arbeidsgivere from '../../tests/mocks/arbeidsgivere';
-import testOrganisasjoner from '../mock/testOrganisasjoner';
+import testOrganisasjoner from '../../mock/testOrganisasjoner';
 
 describe('mapArbeidsgiver', () => {
   it('should map backend data to something useable', () => {
